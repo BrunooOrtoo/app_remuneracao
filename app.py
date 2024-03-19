c1e1aa4f72a0c7d7e0d475286507442f317e4935
BrunooOrtoo <163320480+BrunooOrtoo@users.noreply.github.com>
2024-03-19 08:29:45 -0300

    alteração

--- app.py
+++ app.py
@@ -174,5 +174,9 @@ def get_embed_info():
 def chatbot():
     return render_template('chatbot.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     app.run(debug=True)
