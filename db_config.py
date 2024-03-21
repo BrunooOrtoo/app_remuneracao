f80f70dfb77401c20f1d9aca4ff3b6dc21c0752e
BrunooOrtoo <163320480+BrunooOrtoo@users.noreply.github.com>
2024-03-21 10:28:37 -0300

    alteração

--- db_config.py
+++ db_config.py
@@ -1,5 +1,3 @@
-# db_config.py
-
 import pyodbc
 from dotenv import load_dotenv
 import os
@@ -13,13 +11,25 @@ def get_database_connection():
         database = os.getenv('DB_DATABASE')
         username = os.getenv('DB_USERNAME')
         password = os.getenv('DB_PASSWORD')
-        driver = '{ODBC Driver 17 for SQL Server}'
-        conn_str = 'DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
+        driver = 'ODBC Driver 17 for SQL Server'
+
+        # Adicione os parâmetros de timeout à string de conexão
+        conn_str = (
+            'DRIVER=' + driver + ';'
+            'SERVER=' + server + ';'
+            'DATABASE=' + database + ';'
+            'UID=' + username + ';'
+            'PWD=' + password + ';'
+            'TIMEOUT=30;'  # Defina o tempo limite da conexão para 30 segundos
+            'QUERY_TIMEOUT=60;'  # Defina o tempo limite da consulta para 60 segundos
+        )
+
         conn = pyodbc.connect(conn_str)
         return conn
     except pyodbc.Error as ex:
         # Em vez de retornar render_template, registre o erro para depuração
         print("Erro ao conectar ao banco de dados:", ex)
         return None
+
 
 
